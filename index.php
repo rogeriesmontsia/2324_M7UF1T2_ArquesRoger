54bc392f75af9c2098769bb629394c6bdcf13be0
rogeriesmontsia <rogerarques@iesmontsia.org>
2023-09-23 17:06:36 +0200

    dockeritzar + POO + est. control
    Es transforma en POO i s'afegeix una estructura de control per a diferents casos (no seleccionar operacio, no afegir valors...)

--- index.php
+++ index.php
@@ -1,6 +1,57 @@
 <?php
 session_start();
 
+class Calculadora
+{
+  public static function inputsPlenes($input1, $input2)
+  {
+    return !empty($input1) && !empty($input2);
+  }
+  public static function sumar($input1, $input2)
+  {
+    return $input1 + $input2;
+  }
+  public static function restar($input1, $input2)
+  {
+    return $input1 - $input2;
+  }
+  public static function multiplicar($input1, $input2)
+  {
+    return $input1 * $input2;
+  }
+  public static function dividir($input1, $input2)
+  {
+    if ($input2 != 0) {
+      return $input1 / $input2;
+    } else {
+      return 'Divisió per zero';
+    }
+  }
+  public static function factorial($n)
+  {
+    return ($n == 0) ? 1 : $n * self::factorial($n - 1);
+  }
+  public static function concatenar($input1, $input2)
+  {
+    return $input1 . $input2;
+  }
+  public static function eliminar($input1, $input2)
+  {
+    return str_replace($input2, "", $input1);
+  }
+}
+
+function mostrarResultat($result)
+{
+  echo "<h2>Resultat:</h2>";
+  echo "<p>$result</p>";
+}
+
+function mostrarAlerta($message)
+{
+  echo "<h1>$message</h1>";
+}
+
 // Inicialitza l'array de l'historial si encara no existeix
 if (!isset($_SESSION['history'])) {
   $_SESSION['history'] = array();
@@ -16,89 +67,129 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $result = '';
 
   // Realitza l'operació seleccionada
-  switch ($operation) {
-    case '+':
-      $result = $input1 + $input2;
-      break;
-    case '-':
-      $result = $input1 - $input2;
-      break;
-    case '*':
-      $result = $input1 * $input2;
-      break;
-    case '/':
-      if ($input2 != 0) {
-        $result = $input1 / $input2;
-      } else {
-        $result = 'Divisió per zero';
-      }
-      break;
-    case 'factorial':
-      // Implementa el càlcul del factorial
-      function factorial($n) {
-        return ($n == 0) ? 1 : $n * factorial($n - 1);
-      }
-      $result = factorial($input1);
-      break;
-    case 'concatenate':
-      $result = $input1 . $input2;
-      break;
-    case 'eliminar':
-      $result = str_replace($input2, "", $input1);
-      break;
-    default:
+
+  if ($operation === 'factorial') {
+    if (!empty($input1)) {
+      $result = Calculadora::factorial($input1);
+    } else {
+      mostrarAlerta("Si us plau, introdueix un valor.");
       $result = 'Operació no vàlida';
+    }
+  } else {
+    if (Calculadora::inputsPlenes($input1, $input2)) {
+      switch ($operation) {
+        case '+':
+          $result = Calculadora::sumar($input1, $input2);
+          break;
+        case '-':
+          $result = Calculadora::restar($input1, $input2);
+          break;
+        case '*':
+          $result = Calculadora::multiplicar($input1, $input2);
+          break;
+        case '/':
+          $result = Calculadora::dividir($input1, $input2);
+          break;
+        case 'concatenate':
+          $result = Calculadora::concatenar($input1, $input2);
+          break;
+        case 'eliminar':
+          $result = Calculadora::eliminar($input1, $input2);
+          break;
+        default:
+          $result = 'Operació no vàlida';
+      }
+    }
+
+    if (empty($operation) && empty($input1) && empty($input2)) {
+      mostrarAlerta("Si us plau, selecciona una operació i afegeix dos nombres.");
+      $result = 'Operació no vàlida';
+    }
+    if (empty($operation) && !empty($input1) && !empty($input2)) {
+      mostrarAlerta("Si us plau, selecciona una operacio.");
+      $result = 'Operació no vàlida';
+    }
+    if (!empty($operation) && empty($input1) && empty($input2)) {
+      mostrarAlerta("Si us plau, afegeix dos nombres.");
+      $result = 'Operació no vàlida';
+    }
+    if (!empty($operation) && !empty($input1) && empty($input2)) {
+      mostrarAlerta("Si us plau, afegeix el nombre del segon terme.");
+      $result = 'Operació no vàlida';
+    }
+    if (!empty($operation) && empty($input1) && !empty($input2)) {
+      mostrarAlerta("Si us plau, afegeix el nombre del primer terme.");
+      $result = 'Operació no vàlida';
+    } else {
+      //mostrarAlerta("Si us plau, introdueix dos valors.");
+      //$result = 'Operació no vàlida';
+    }
   }
 
 
-
   // Afegir l'operació a l'històric
-  $operationString = "$input1 $operation $input2 = $result";
-  array_push($_SESSION['history'], $operationString);
+  if ($result !== 'Operació no vàlida') {
+    $operacioRealitzada = "$input1 $operation $input2 = $result";
+    array_push($_SESSION['history'], $operacioRealitzada);
+  }
 }
 ?>
 
 <!DOCTYPE html>
 <html>
+
 <head>
-  <title>Calculadora</title>
+  <title>🧙Calculadora</title>
+  <link rel="stylesheet" href="styles.css">
 </head>
+<header>
+  <h1>🧙Calculadora</h1>
+</header>
+
 <body>
-  <h1>Calculadora</h1>
+
 
   <form action="" method="post">
     <label for="input1">Primer valor:</label>
     <input type="text" id="input1" name="input1"><br><br>
-
-    <label for="operacio">Operació:</label>
-    <select id="operacio" name="operacio">
-      <option value="+">Suma (+)</option>
-      <option value="-">Resta (-)</option>
-      <option value="*">Multiplicació (*)</option>
-      <option value="/">Divisió (/)</option>
-      <option value="factorial">Factorial</option>
-      <option value="concatenate">Concatenar strings</option>
-      <option value="eliminar">Eliminar part string</option>
-    </select><br><br>
-
+    <label for="operacio">Operació a realitzar:</label><br>
+    <input type="radio" id="suma" name="operacio" value="+">
+    <label for="operacio">➕</label><br>
+    <input type="radio" id="resta" name="operacio" value="-">
+    <label for="operacio">➖</label><br>
+    <input type="radio" id="multiplicacio" name="operacio" value="*">
+    <label for="operacio">✖️</label><br>
+    <input type="radio" id="divisio" name="operacio" value="/">
+    <label for="operacio">➗</label><br>
+    <input type="radio" id="factorial" name="operacio" value="factorial">
+    <label for="operacio">Factorial</label><br>
+    <input type="radio" id="concatena" name="operacio" value="concatenate">
+    <label for="operacio">Concatenar strings</label><br>
+    <input type="radio" id="elimina" name="operacio" value="eliminar">
+    <label for="operacio">Eliminar part string</label><br><br>
     <label for="input2">Segon valor:</label>
     <input type="text" id="input2" name="input2"><br><br>
 
-    <button type="submit" name="calcular">Calcular</button>
+    <button class="boto" type="submit" name="calcular">Calcular🪄</button>
   </form>
   <?php
-    // Mostra el resultat
-    echo "<h2>Resultat:</h2>";
-    echo "<p>$result</p>";
+  // Mostra el resultat només si l'operació és vàlida
+  if ($result !== 'Operació no vàlida') {
+    mostrarResultat($result);
+  }
   ?>
   <h2>Historial d'operacions:</h2>
-  <ul id="history">
+  <div id="history">
     <?php
-    
-    foreach ($_SESSION['history'] as $operation) {
-      echo "<li>$operation</li>";
+    // Obtenim el total d'operacions realitzades
+    $total_operacions = count($_SESSION['history']);
+    // Itera a través del historial en ordre invers
+    for ($i = $total_operacions - 1; $i >= 0; $i--) {
+      $operacio = $_SESSION['history'][$i];
+      echo "$operacio<br>";
     }
     ?>
-  </ul>
+  </div>
 </body>
+
 </html>
